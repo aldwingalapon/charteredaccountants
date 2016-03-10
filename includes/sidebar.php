f8d91b356abca79d8d88106b670dc45a903f32bd
edwardsomera <gigsterdeveloper@gmail.com>
2016-03-10 21:04:46 +0800

    31020162104 sidebar update

---
+++ includes/sidebar.php
@@ -0,0 +1,40 @@
+<div class="avatar">
+	<img src="images/avatar2.png" alt="avatar"/>
+	<h1>Joshua Bretag</h1>
+	<h2 class="ribbon">
+   	<strong class="ribbon-content">Provisional member</strong>
+	</h2>
+</div>
+<div class="row">
+  <div class="sidebar-nav">
+    <div class="navbar navbar-default" role="navigation">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-navbar-collapse">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <span class="visible-xs navbar-brand">Sidebar menu</span>
+      </div>
+      
+      <div class="navbar-collapse collapse sidebar-navbar-collapse">
+        <ul class="nav navbar-nav">
+          <li class="nav-button-dash"><a href="#">My DASHBOARD</a></li>
+          <li class="nav-button-enrolled"><a href="#">my enrolled programs</a></li>
+          <li class="nav-button-activity"><a href="#">my registered activities</a></li>
+          <li class="nav-button-cases"><a href="#">my cases</a></li>
+          <li class="dropdown nav-button-link">
+            <a href="#demo4" class="list-group-item-success" data-toggle="collapse" data-parent="#MainMenu">Links <span class="caret"></span></a>
+    					<div class="collapse" id="demo4">
+      					<a href="" class="list-group-item">CA Program Links &amp; Resources</a>
+      					<a href="" class="list-group-item">CA Program Links &amp; Resources</a>
+      					<a href="" class="list-group-item">CA Program Links &amp; Resources</a>
+      					<a href="" class="list-group-item">CA Program Links &amp; Resources</a>
+    					</div>
+          </li>
+        </ul>
+      </div><!--/.nav-collapse -->
+    </div>
+  </div>
+</div>
