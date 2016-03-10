07aa1374596e047919ab3bb9bd715d6494f88a38
Aldwin Galapon <aldwin.galapon@gmail.com>
2016-03-10 22:04:03 +0800

    Updates 031020162204

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 	<div class="content-container" id="page-content">
 		<div class="row-fluid">
 			<div id="left-sidebar" class="col-md-2 sidebar">
-        <?php include("includes/sidebar.php");?>
-      </div>
+				<?php include("includes/sidebar.php");?>
+			</div>
 			<div class="col-md-10 main-content">
 				<h1>Main Content Here</h1>
 			</div>
