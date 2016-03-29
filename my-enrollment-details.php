0b51515133adec37d656bf133c2c52613b74e0fb
Aldwin Galapon <aldwin.galapon@gmail.com>
2016-03-29 15:14:19 +0800

    My Enrollment Details Page 151403292016

---
+++ my-enrollment-details.php
@@ -0,0 +1,54 @@
+<?php include("includes/header.php");?>
+
+	<div class="content-container" id="page-content">
+		<div class="row-fluid">
+			<div id="left-sidebar" class="col-md-2 sidebar">
+				<?php include("includes/sidebar.php");?>
+			</div>
+			<div class="col-md-10 main-content dashboard">
+				<div class="table-header">Graduate Diploma of Chartered Accounting - Details</div>
+				<div class="info-message info">Module Selection: Please select atleast 4 option below</div>
+				<table class="education">
+					<thead>
+						<tr>
+							<th>Selection</th>
+							<th>Subject Name</th>
+							<th>Essential</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td><input type="checkbox" checked> Please check me pretty please</td>
+							<td>Lorem Ipsum Dolor Itset</td>
+							<td>Core</td>
+						</tr>
+						<tr>
+							<td><input type="checkbox"> Please check me pretty please</td>
+							<td>Lorem Ipsum Dolor Itset</td>
+							<td>Elective</td>
+						</tr>
+						<tr>
+							<td><input type="checkbox" checked> Please check me pretty please</td>
+							<td>Lorem Ipsum Dolor Itset</td>
+							<td>Elective</td>
+						</tr>
+						<tr>
+							<td><input type="checkbox"> Please check me pretty please</td>
+							<td>Lorem Ipsum Dolor Itset</td>
+							<td>Elective</td>
+						</tr>
+						<tr>
+							<td><input type="checkbox" checked> Please check me pretty please</td>
+							<td>Lorem Ipsum Dolor Itset</td>
+							<td>Core</td>
+						</tr>
+					</tbody>
+				</table>
+				<div class="table-footer"><button class="btn btn-default navigation prev-btn" title="Back">Back</button><button class="btn btn-default navigation next-btn" title="Next">Next</button></div>
+			</div>
+			<div class="clearfix">
+			</div>
+		</div>
+	</div>
+
+<?php include("includes/footer.php");?>
