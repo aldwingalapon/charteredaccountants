2bdad525a5772f1453fb2a492aaba1df1429ea6c
edwardsomera <gigsterdeveloper@gmail.com>
2016-03-29 14:29:45 +0800

    32920161429 my-enrollment-course-selection.php page

---
+++ my-enrollment-course-selection.php
@@ -0,0 +1,40 @@
+<?php include("includes/header.php");?>
+
+	<div class="content-container" id="page-content">
+		<div class="row-fluid">
+			<div id="left-sidebar" class="col-md-2 sidebar">
+				<?php include("includes/sidebar.php");?>
+			</div>
+			<div class="col-md-10 main-content">
+				<h2 class="enroll-recent-title">Graduate Diploma of Chartered Accounting - Course Selection</h2>
+				<table class="enrollment">
+					<thead>
+						<tr>
+							<td>Course Name</td>
+							<td>Commencement</td>
+							<td>Enrollment Open</td>
+							<td>Enrollment Cloe</td>
+							<td>Action</td>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>AAA316</td>
+							<td>19/09/2016</td>
+							<td>9/03/2016	</td>
+							<td>22/08/2016</td>
+							<td><button type="button" class="btn btn-default enroll">Enroll<span><img src="images/enroll-arrow.png" alt="enroll" width="25" height="25"></span></button></td>
+						</tr>
+						
+					</tbody>
+				</table>
+				
+				<div class="table-footer"><button class="btn btn-default view prev">Back</button>
+				<button class="btn btn-default view next">Next</button></div>
+			</div>
+			<div class="clearfix">
+			</div>
+		</div>
+	</div>
+
+<?php include("includes/footer.php");?>
