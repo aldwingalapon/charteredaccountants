f39cfd69e0926c6a5abbc724a8034766ba2c9819
edwardsomera <gigsterdeveloper@gmail.com>
2016-03-29 13:37:13 +0800

    32920161336 my-enrollment.php page

---
+++ my-enrollment.php
@@ -0,0 +1,130 @@
+<?php include("includes/header.php");?>
+
+	<div class="content-container" id="page-content">
+		<div class="row-fluid">
+			<div id="left-sidebar" class="col-md-2 sidebar">
+				<?php include("includes/sidebar.php");?>
+			</div>
+			<div class="col-md-10 main-content">
+				<h2 class="enroll-recent-title">Graduate Diploma of Chartered Accounting</h2>
+				<table class="program">
+					<thead>
+						<tr>
+							<td>Module</td>
+							<td>Course Name</td>
+							<td>Status</td>
+							<td>Paid</td>
+							<td>Score</td>
+							<td>Result</td>
+							<td>Action</td>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>AAA</td>
+							<td>Fundamentals on Programming</td>
+							<td>Active</td>
+							<td>Yes</td>
+							<td>n/a</td>
+							<td>n/a</td>
+							<td><button type="button" class="btn btn-default enroll">Enroll<span><img src="images/enroll-arrow.png" alt="enroll" width="25" height="25"></span></button></td>
+						</tr>
+						<tr>
+							<td>FIN</td>
+							<td>Fundamentals on Programming</td>
+							<td>InActive</td>
+							<td>No</td>
+							<td>n/a</td>
+							<td>n/a</td>
+							<td><button type="button" class="btn btn-default enroll">Enroll<span><img src="images/enroll-arrow.png" alt="enroll" width="25" height="25"></span></button></td>
+						</tr>
+						<tr>
+							<td>AAA</td>
+							<td>Fundamentals on Programming</td>
+							<td>Active</td>
+							<td>Yes</td>
+							<td>n/a</td>
+							<td>n/a</td>
+							<td><button type="button" class="btn btn-default enroll">Enroll<span><img src="images/enroll-arrow.png" alt="enroll" width="25" height="25"></span></button></td>
+						</tr>
+						<tr>
+							<td>FIN</td>
+							<td>Fundamentals on Programming</td>
+							<td>InActive</td>
+							<td>No</td>
+							<td>n/a</td>
+							<td>n/a</td>
+							<td><button type="button" class="btn btn-default enroll">Enroll<span><img src="images/enroll-arrow.png" alt="enroll" width="25" height="25"></span></button></td>
+						</tr>
+						<tr>
+							<td>AAA</td>
+							<td>Fundamentals on Programming</td>
+							<td>Active</td>
+							<td>Yes</td>
+							<td>n/a</td>
+							<td>n/a</td>
+							<td><button type="button" class="btn btn-default enroll">Enroll<span><img src="images/enroll-arrow.png" alt="enroll" width="25" height="25"></span></button></td>
+						</tr>
+						
+					</tbody>
+				</table>
+				<div class="table-footer"><button class="btn btn-default view view-all">View All</button></div>
+
+				<h2 class="enroll-recent-title">My Recent Module Enrollments</h2>
+				<table class="program">
+					<thead>
+						<tr>
+							<td>Module</td>
+							<td>Commencement</td>
+							<td>Enrollment Start Date</td>
+							<td>Enrollment End Date</td>
+							<td>Status</td>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>Learn HTML5 Programming From Scratch<br>Graduate Diploma of HTML5</td>
+							<td>Lorem Ipsum Dolor Itset</td>
+							<td>05/05/2016</td>
+							<td>06/05/2016</td>
+							<td><button type="button" class="btn btn-default in-progress">In progress</button></td>
+						</tr>
+						<tr>
+							<td>Learn HTML5 Programming From Scratch<br>Graduate Diploma of HTML5</td>
+							<td>Lorem Ipsum Dolor Itset</td>
+							<td>05/05/2016</td>
+							<td>06/05/2016</td>
+							<td><button type="button" class="btn btn-default stop">Stop</button></td>
+						</tr>
+						<tr>
+							<td>Learn HTML5 Programming From Scratch<br>Graduate Diploma of HTML5</td>
+							<td>Lorem Ipsum Dolor Itset</td>
+							<td>05/05/2016</td>
+							<td>06/05/2016</td>
+							<td><button type="button" class="btn btn-default in-progress">In progress</button></td>
+						</tr>
+						<tr>
+							<td>Learn HTML5 Programming From Scratch<br>Graduate Diploma of HTML5</td>
+							<td>Lorem Ipsum Dolor Itset</td>
+							<td>05/05/2016</td>
+							<td>06/05/2016</td>
+							<td><button type="button" class="btn btn-default in-progress">In progress</button></td>
+						</tr>
+						<tr>
+							<td>Learn HTML5 Programming From Scratch<br>Graduate Diploma of HTML5</td>
+							<td>Lorem Ipsum Dolor Itset</td>
+							<td>05/05/2016</td>
+							<td>06/05/2016</td>
+							<td><button type="button" class="btn btn-default in-progress">In progress</button></td>
+						</tr>
+						
+					</tbody>
+				</table>
+				<div class="table-footer"><button class="btn btn-default view view-all">View All</button></div>
+			</div>
+			<div class="clearfix">
+			</div>
+		</div>
+	</div>
+
+<?php include("includes/footer.php");?>
